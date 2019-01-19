94f1d9766ba818c4ee89827decacebe1687d8462
trungnguyen <trungnguyen2171992@gmail.com>
2019-01-18 22:39:18 -0800

    Complete getting classifications from caffeModel

--- FlowersGuru/ViewController.swift
+++ FlowersGuru/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     let imagePicker = UIImagePickerController()
-
+    
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
@@ -21,20 +24,44 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.allowsEditing = true
         imagePicker.sourceType = .camera
         
- 
+        
     }
-
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let userPickedImage = info[UIImagePickerController.InfoKey.editedImage]
-        
-        imageView.image = userPickedImage as? UIImage
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+            
+            
+            guard let convertedCIImage = CIImage(image: userPickedImage) else {fatalError("cannot convert to CIImage")}
+            detect(image: convertedCIImage)
+            
+            imageView.image = userPickedImage
+            
+        }
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage){
         
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {fatalError("cannot import model")}
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            self.navigationItem.title = classification?.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        }
+        catch{
+            print(error)
+        }
         
     }
-
+    
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
-
+        
         present(imagePicker,animated: true, completion: nil)
         
     }
